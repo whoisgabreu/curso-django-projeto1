5c6c67e125530effaeed1be24cbfb058506caa22
Gabriel Lucas <gabreumartins02@gmail.com>
2024-12-13 10:16:59 -0300

    aaaa2

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse("CONTATO")
