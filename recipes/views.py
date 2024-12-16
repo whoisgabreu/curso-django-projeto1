ee0e60c1b1653687cb74c26b3dc74abaacec168f
Gabriel Lucas <gabreumartins02@gmail.com>
2024-12-16 14:03:05 -0300

    Removidos os URLs de Contato e Sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html')
-
-def contato(request):
-    return render(request, "me-apague/temp.html")
-
-def sobre(request):
-    return HttpResponse("SOBRE")
